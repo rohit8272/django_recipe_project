27068ddd301767c0e88ad73b48b314e0273f3e04
rohit8272 <sharmarohit637782@gmail.com>
2024-02-15 10:43:12 +0530

    add authenticate features

--- core/recipes/urls.py
+++ core/recipes/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('' ,recipe_add ,name='recipe_add' ),
+    path('' , home ,name='home' ),
+    path('recipes/' ,recipe_add ,name='recipe_add' ),
     path('recipe_delete/<id>/' ,recipe_delete , name='recipe_delete' ),
     path('recipe_update/<id>/' ,recipe_update, name='recipe_delete' ),
-    
+    path('register/' , register ,name="register"),
+    path('login/' , login_page ,name="login"),
+    path('logout/' , logout_page ,name="logout_page"),
+
 ]
--- core/recipes/views.py
+++ core/recipes/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render ,redirect
 from .models import Recipe
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate , login ,logout
+from django.contrib.auth.decorators import login_required
 
+def home(request):
+    return render(request , 'index.html')
+
+@login_required(login_url='/login')
 def recipe_add(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -16,7 +24,7 @@ def recipe_add(request):
     context = {'recipes' : recipes}      
     return render(request, 'recipes_ui.html' , context)
 
-
+@login_required(login_url='/login')
 def recipe_update(request ,id):
     query_set = Recipe.objects.get(id = id)
     
@@ -33,13 +41,64 @@ def recipe_update(request ,id):
 
         query_set.save()  
 
-        return redirect('/') 
+        return redirect('/recipes') 
 
     context = {'recipes' : query_set} 
     return render(request , 'update_recipe.html' , context)
 
-
+@login_required(login_url='/login')
 def recipe_delete(request , id):
     query_set = Recipe.objects.get(id = id)
     query_set.delete()
-    return redirect('/')
+    return redirect('/recipes')
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    
+        user = User.objects.filter(username = username).exists()
+
+        if not user:
+           messages.info(request, "user not exist")
+           return redirect('/register')
+        
+        user = authenticate(username = username , password = password)
+        if user is None:
+            messages.error(request, "invalid credentials")
+            return redirect('/login')
+        else:
+            login(request,user)
+            return redirect('/recipes')
+
+    return render(request , 'login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('/login')
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.info(request, "user already exist")
+            return redirect('/register')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username
+        )
+
+        user.set_password(password)
+        user.save()
+        messages.info(request, "user registered successfully !")
+        return redirect('/register')
+
+    return render(request , 'register.html')
